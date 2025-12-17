70558e8c08227b724c2440044753ce4013fb4a73
yaniyani <yanilee_1114@outlook.com>
2025-12-17 15:02:20 +0700

    add protocol and function

--- Sources/NetworkLibrary/NetworkLibrary.swift
+++ Sources/NetworkLibrary/NetworkLibrary.swift
@@ -6,8 +6,7 @@ import Foundation
 @available(macOS 10.15.0, *)
 @available(iOS 13.0.0, *)
 protocol APIClientProtocol {
-    func sendRequest<T: Decodable>(endpoint: Endpoint) async throws -> T
-    func sendDynamicRequest(endpoint: Endpoint) async throws -> Any
+    func sendRequest(endpoint: Endpoint) async throws -> Data
 }
 
 @available(macOS 10.15.0, *)
@@ -20,16 +19,12 @@ final public class APIClient: APIClientProtocol {
         self.session = URLSession.shared
     }
 
-    public func sendRequest<T>(endpoint: Endpoint) async throws -> T where T : Decodable {
+    public func sendRequest(endpoint: Endpoint) async throws -> Data {
         var request = URLRequest(url: endpoint.url, timeoutInterval: Double.infinity)
         request.httpMethod = endpoint.method.rawValue
 
-        print("endpoint :", endpoint.url)
-        print("method :", endpoint.method.rawValue)
-
         if let header = endpoint.headers {
             request.allHTTPHeaderFields = setHttpHeader(endpoint.encoding, with: header)
-            print("header :", request.allHTTPHeaderFields ?? [:])
         }
 
         if let body = endpoint.body {
@@ -42,55 +37,8 @@ final public class APIClient: APIClientProtocol {
             throw URLError(.badServerResponse)
         }
 
-        do {
-            let formatter = DateFormatter()
-            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssXXX"
-            formatter.locale = Locale(identifier: "en_US_POSIX")
-
-            let decoder = JSONDecoder()
-            decoder.dateDecodingStrategy = .formatted(formatter)
-
-            let decoded = try decoder.decode(T.self, from: data)
-            print("result :", decoded)
-            return decoded
-        } catch {
-            print("decode error:", error)
-            throw URLError(.cannotDecodeContentData)
-        }
+        return data
     }
-
-    public func sendDynamicRequest(endpoint: Endpoint) async throws -> Any {
-        var request = URLRequest(url: endpoint.url, timeoutInterval: Double.infinity)
-        request.httpMethod = endpoint.method.rawValue
-
-        print("endpoint :", endpoint.url)
-        print("method :", endpoint.method.rawValue)
-
-        if let header = endpoint.headers {
-            request.allHTTPHeaderFields = setHttpHeader(endpoint.encoding, with: header)
-            print("header :", request.allHTTPHeaderFields ?? [:])
-        }
-
-        if let body = endpoint.body {
-            request.httpBody = setHttpBody(endpoint.encoding, with: body)
-        }
-
-        let (data, response) = try await session.data(for: request)
-
-        guard response is HTTPURLResponse else {
-            throw URLError(.badServerResponse)
-        }
-
-        do {
-            let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
-            print("json:", json)
-            return json
-        } catch {
-            print("decode error:", error)
-            throw URLError(.cannotDecodeContentData)
-        }
-    }
-
 
     private func setHttpHeader(_ encoding: BodyEncoding, with header: [String: String]) -> [String: String] {
         var httpHeader: [String: String] = [:]
